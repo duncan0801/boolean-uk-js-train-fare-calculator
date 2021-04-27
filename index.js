004ecc70debb8646bff60f581ab7daea668ddc8b
Duncan Magill <duncanmagill@hotmail.co.uk>
2021-04-27 16:16:53 +0100

    finished challenge

--- index.js
+++ index.js
@@ -10,15 +10,53 @@ We're going to rewrite our program so that the user chooses a starting point and
 
 You can come up with the stations' names and distances. Try also to code it so that you only need to change the price per km in one place, and all the stations should have the updated price
 */
+
+
+
+//MAIN TASK
 const juniorPassengerAge = 18
 const seniorPassengerAge = 65
 const pricePerKM = 0.21
 
 let yourAge = Number(prompt(`What is your age?`))
-let yourDistance = Number(prompt(`How far are you travelling?`))
+// let yourDistance = Number(prompt(`How far are you travelling?`))
+let startDestination = prompt(`What is your starting station?:
+Edinburgh, 
+Durham,
+Manchester,
+Liverpool,
+London`)
+let endDestination = prompt(`What is your final station?:
+Durham,
+Manchester,
+Liverpool,
+London`)
+
+if (startDestination === "Edinburgh")
+    startDestination = 0
+else if (startDestination === "Durham")
+    startDestination = 100
+else if (startDestination === "Manchester")
+    startDestination = 250
+else if (startDestination === "Liverpool")
+    startDestination = 300
+else if (startDestination === "London")
+    startDestination = 450
+
+    if (endDestination === "Edinburgh")
+    endDestination = 0
+else if (endDestination === "Durham")
+    endDestination = 100
+else if (endDestination === "Manchester")
+    endDestination = 250
+else if (endDestination === "Liverpool")
+    endDestination = 300
+else if (endDestination === "London")
+    endDestination = 450
 
 
-let ticketPrice = (pricePerKM * yourDistance).toFixed(2)
+let totalJourney = endDestination - startDestination
+let ticketPrice = (pricePerKM * totalJourney).toFixed(2)
 
 console.log(`Normal Ticket Price: £${ticketPrice}`)
 alert(`Normal Ticket Price: £${ticketPrice}`)
@@ -36,3 +74,9 @@ else if (yourAge > seniorPassengerAge) {
     else
     console.log(`Your Ticket Price: £${ticketPrice}`)
     alert(`Your Ticket Price: £${ticketPrice.toFixed(2)}`)
+
+
+
+
+
+
