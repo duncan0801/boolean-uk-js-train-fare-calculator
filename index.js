9624340bd296b932f441b5f7877ab534a24d2f3f
Duncan Magill <duncanmagill@hotmail.co.uk>
2021-04-27 15:09:45 +0100

    made the code layout clearer

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ We're going to rewrite our program so that the user chooses a starting point and
 
 You can come up with the stations' names and distances. Try also to code it so that you only need to change the price per km in one place, and all the stations should have the updated price
 */
-const pricePerKM = 0.21
-let yourAge = Number(prompt(`What is your age?`))
-let yourDistance = Number(prompt(`How far are you travelling?`))
 const juniorPassengerAge = 18
 const seniorPassengerAge = 65
+const pricePerKM = 0.21
+
+let yourAge = Number(prompt(`What is your age?`))
+let yourDistance = Number(prompt(`How far are you travelling?`))
+
 
 let ticketPrice = pricePerKM * yourDistance
 
