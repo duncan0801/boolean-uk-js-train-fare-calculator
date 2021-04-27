60e234d5c86a461ee0c67a09349eb353b950915a
Duncan Magill <duncanmagill@hotmail.co.uk>
2021-04-27 14:34:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/* 
+Instructions
+- Ask the user for their age and how far they are travelling (in km)
+- The price per travelled km will be £0.21
+- Junior passengers (under 18) get a 20% discount
+- Senior passengers (over 65) get a 40% discount
+
+Challenge
+We're going to rewrite our program so that the user chooses a starting point and a destination from a list of stations, besides inputting their age and then get the ticket price. 
+
+You can come up with the stations' names and distances. Try also to code it so that you only need to change the price per km in one place, and all the stations should have the updated price
+*/
+
+let yourAge = Number(prompt(What is your age?))
+let yourDistance = Number(prompt(How far are you travelling?))
+
+if (yourAge < 18) {
+    
+}
